3624e85d11bb7c6bd9f995f2aca971dee5efb4c0
Cogno_Barco <cognokill@gmail.com>
2017-08-07 22:46:43 +0200

    Hangar MK1
    Fondamenta sistema di Hangar. Sviluppati:
    Mura dell' hangar con pad dove vengono scelte le astronavi.
    Animazione oscillazione astronave.
    Animazione cambio astronave

---
+++ Assets/Stages/Hangar/WaveAndRotateAnimationShips.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveAndRotateAnimationShips : MonoBehaviour {
+
+    public AnimationCurve heightCurve;
+    public float heightAdd;
+    public Vector3 startPos;
+    public float rotSpeed;
+
+    private float minHeight;
+   
+	// Use this for initialization
+	void Start () {
+        minHeight = transform.position.y;
+    }
+
+
+
+	
+
+	// Update is called once per frame
+	void Update () {
+        //float up and down
+        float newHeight = minHeight + heightCurve.Evaluate(Time.time) * heightAdd;
+        Vector3 newPos = new Vector3(transform.position.x, newHeight, transform.position.z);
+        transform.position = newPos;
+
+        //rotate over time
+        transform.Rotate(new Vector3(0,0,1) * rotSpeed * Time.deltaTime);
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(startPos, 1);
+        Gizmos.DrawWireSphere(startPos + Vector3.up * heightAdd, 1);
+    }
+
+}
