acfea207bf04bb0cc073402b6751d8197ef44ad7
Cogno_Barco <cognokill@gmail.com>
2017-08-09 20:50:53 +0200

    Game Over text + main menu button
    quando si muore ora compaiono una scritta che dice "Game Over" e un pulsante per tornare al menù principale

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Canvas canvas;
+    public float timer;
+
+    float _timer;
+	// Use this for initialization
+	void Start () {
+        canvas.enabled = false;
+
+        _timer = timer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.Find("Player");
+
+        if(player == null)
+        {
+            _timer -= Time.deltaTime;
+        }
+
+        if(_timer <= 0)
+        {
+            canvas.enabled = true;
+        }
+	}
+}
